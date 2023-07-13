33f51cecb260ce320f89b28139628c32f81e8c5f
vikitaotiz <victorotieno598@gmail.com>
2023-07-13 17:09:03 +0300

    Fix sales stats

---
+++ assets/use-quasar.b3f68725.js
@@ -0,0 +1,1 @@
+import{i as a,bZ as r}from"./index.51a259cb.js";function u(){return a(r)}export{u};
---
+++ assets/vuequery.7f51bcc5.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.3312b787.js";import"./index.51a259cb.js";var o=({app:e})=>{e.use(r)};export{o as default};
